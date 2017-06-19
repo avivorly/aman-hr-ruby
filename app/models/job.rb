46cafd72641882e0b35acab31fa6ab98319864e9
avivorly <aviv.orly@netvision.net.il>
2017-06-19 22:03:27 +0300

    hy

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,8 @@
 class Job < ApplicationRecord
+  has_many :jtags
+
+  Soldier.all.sort_by do |s|
+    jtags.count{|j|  s.stags.find{|t| t.value == j.value && t.key == j.key}}
+  end.reverse
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619172258) do
+ActiveRecord::Schema.define(version: 20170619184047) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jtags", force: :cascade do |t|
+    t.string   "key"
+    t.string   "value"
+    t.integer  "soldier_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["soldier_id"], name: "index_jtags_on_soldier_id"
   end
 
   create_table "soldiers", force: :cascade do |t|
