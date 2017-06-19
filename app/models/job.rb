36d401ceda7c2f8240151b2e103f98be5f6228f7
avivorly <aviv.orly@netvision.net.il>
2017-06-19 23:36:03 +0300

    fdgrt

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,10 +3,7 @@ class Job < ApplicationRecord
 
   def match_sol
     Soldier.all.map do |s|
-      [s,jtags.to_a.count{|j|  s.stags.find{|t| t.value == j.value && t.key == j.key}}]
-    end.sort_by{|_,n|n}.reverse
+      [jtags.to_a.count{|j|  s.stags.find{|t| t.value == j.value && t.key == j.key}},jtags.length,s.attributes]
+    end.select{|a,_,_|a > 0}.sort_by{|_,n|n}
   end
-
-
-
 end
