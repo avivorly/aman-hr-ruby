6d1a57b1f6d8c765b793f375c232857d62d02103
avivorly <aviv.orly@netvision.net.il>
2017-06-20 03:55:37 +0300

    UI for list and details pages

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,7 @@ class Job < ApplicationRecord
 
   def match_sol
     Soldier.all.map do |s|
-      [jtags.to_a.count{|j|  s.stags.find{|t| t.value == j.value && t.key == j.key}},jtags.length,s.attributes]
+      [jtags.to_a.count{|j|  s.stags.find{|t| t.value == j.value && t.key.gsub(/[0-9]/,'') == j.key.gsub(/[0-9]/,'') && j.value != ''}},jtags.where('value != ""').count,s.attributes]
     end.select{|a,_,_|a > 0}.sort_by{|_,n|n}
   end
 end
